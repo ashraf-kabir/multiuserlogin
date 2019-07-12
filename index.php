15d3d6dc1e62d48db6105153811f2718e0032cb3
Ashraf Kabir <s.m.ashraf.kabir@g.bracu.ac.bd>
2019-07-12 20:27:07 +0600

    changes

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 session_start();
 $conn=mysqli_connect('localhost','root','','codenair');
 //Getting Input value
-if(isset($_POST['login'])) {
+if (isset($_POST['login'])) {
     $username=mysqli_real_escape_string($conn,$_POST['username']);
     $password=mysqli_real_escape_string($conn,$_POST['password']);
     
-    if(empty($username)&&empty($password)) {
+    if (empty($username)&&empty($password)) {
         $error= 'Fileds are Mandatory';
-    } else{
+    } else {
         //Checking Login Detail
         $result=mysqli_query($conn,"SELECT*FROM user WHERE username='$username' AND password='$password'");
         $row=mysqli_fetch_assoc($result);
@@ -33,9 +33,9 @@ if(isset($_POST['login'])) {
                 break;
             }
         } else{
- $error='Your PassWord or UserName is not Found';
- }
-}
+            $error='Your PassWord or UserName is not Found';
+        }
+    }
 }
 ?>
 <html>
@@ -43,23 +43,23 @@ if(isset($_POST['login'])) {
 <title>PHP MySQL Role Based Access Control</title>
 </head>
 <div align="center">
-<h3>PHP MySQL Role Based Access Control</h3>
-<form method="POST" action="">
-<table>
-   <tr>
-     <td>UserName:</td>
-  <td><input type="text" name="username"/></td>
-   </tr>
-   <tr>
-     <td>PassWord:</td>
-  <td><input type="text" name="password"/></td>
-   </tr>
-   <tr>
-     <td></td>
-  <td><input type="submit" name="login" value="Login"/></td>
-   </tr>
-</table>
-</form>
-<?php if(isset($error)){ echo $error; }?>
+    <h3>PHP MySQL Role Based Access Control</h3>
+    <form method="POST" action="">
+        <table>
+            <tr>
+                <td>UserName:</td>
+                <td><input type="text" name="username"/></td>
+            </tr>
+            <tr>
+                <td>PassWord:</td>
+                <td><input type="text" name="password"/></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input type="submit" name="login" value="Login"/></td>
+            </tr>
+        </table>
+    </form>
+    <?php if(isset($error)){ echo $error; }?>
 </div>
 </html>
